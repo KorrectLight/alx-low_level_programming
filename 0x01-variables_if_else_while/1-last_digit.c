efe7172da698ce03e0141d9a917eb854ff9dd357
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-03 12:52:17 -0700

    Low level C programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,9 +5,9 @@
 /* This program is going to print the last digit */
 
 /**
- * main - This contains all main code 
- * Return: void
- */
+* main - This contains all main code 
+* Return: void
+*/
 int main(void)
 {
 	int n;
@@ -20,15 +20,14 @@ int main(void)
 
 	a = n % 10;
 
-	if (a > 5 )
+	if (a > 5)
 	printf("Last digit of %d is %d and is greater than 5\n", n, a);
 
-	else if (a < 6 && a !=0)
-	printf("Last digit of %d is %d and is less than 6\n", n, a);
+	else if  (a < 6 && a != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 
 	else
 	printf("Last digit of %d is %d and is 0\n", n, a);
 
-	return(0);
-
+	return (0);
 }	
