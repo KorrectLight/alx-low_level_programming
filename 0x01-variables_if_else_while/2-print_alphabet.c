0d4f58c2f5ed3feb3453d3f4b7860e7bf20b2ff2
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-03 13:08:17 -0700

    Low level C programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - where all function goes
+ * Return: void
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+
+}
+
