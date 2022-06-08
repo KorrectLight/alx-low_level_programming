ccdcd5b977a1347b3d89b4c1ee08f2fa8fc164d4
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-08 12:34:24 -0700

    Low level C programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * prnt_alphabet - Print alphabet in lowercase.
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("all your main code goes here");
+	return (0);
+
+	void prnt_alphabet(void)
+	{
+		int i;
+
+		for (i = 'a'; i <= 'z'; i++)
+			{
+			putchar(i);
+			}
+			putchar('\n');
+
+	}
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * prnt_alphabet_x10 - Print the alphabet 10 times in lowercase.
+ *
+ * Return (0)
+ */
+
+int main(void)
+{
+	printf("all your main code goes here");
+	return (0);
+
+	void print_alphabeth_x10(void);
+	{
+		int a;
+
+		for (a = 0; a < 10; a++)
+		{
+			int i;
+
+			for (i = 'a'; i <= 'z'; i++)
+			{	
+			putchar(i);
+			putchar('\n');
+			}
+		}	
+	}
+}
