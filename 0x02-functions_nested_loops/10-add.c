a322042c3cec60c8b341f17703a492e84dbb6927
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-08 16:47:18 -0700

    Low level C programming

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,7 @@
  * add - main code
  * @a: first input
  * @b: second input
- * Return; result of the addition of the two input
+ * Return: result of the addition of the two input
  */
 
 int add(int a, int b)
