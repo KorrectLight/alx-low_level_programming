e055e3bdd1e91751de70a7ac93286d7cd6968f49
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-08 18:25:43 -0700

    low level programming

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-	int a, b;
+	int a;
+	int b;
 
-	b = 0
+	b = 0;
 
 	for (a = 0; a < 1024; a++)
-		{
-		if ((a % 3) == 0 || (a % 5) == 0)
-		b = b + a
-		}
-	printf("%dn", b);
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+			b = b + a
+	}
+	printf("%d\n", b);
 	return (0);
 }
