7e8e66b96934419a11ee48f9f12be7e15fab0f8c
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-08 17:29:48 -0700

    Low level C programming

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print natural numbers below 1024 that are multiples of 3  or 5
+ * Return: (0)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+		{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		b += q;
+		}
+	printf("%dn" , b);
+	return (0);
+}
