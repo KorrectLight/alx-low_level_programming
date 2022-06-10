eadb80879e9d77ec237e168bc23e286b68384633
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-09 17:05:19 -0700

    low level programming

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prime numbers
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-	long num = 612852475143;
-	long divisor = 2;
-	long larg_prim = 0;
+	int i;
 
-	while (num != 1)
-	{
-		if (num % divisor == 0)
-		{
-			num = num / divisor;
-			larg_prim = divisor;
-		}
-		divisor += 1;
-	}
-	printf("%ld\n", larg_prim);
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
