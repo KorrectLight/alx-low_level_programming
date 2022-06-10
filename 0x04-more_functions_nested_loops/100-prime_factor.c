2a4ac59744343e5e8acce68883f002e3bee4fb75
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-09 17:30:19 -0700

    low level programming

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,77 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/*
- * This code prints the highest factor
- * of a number which is also a prime
- * number
- */
 
 /**
- * prime - main code here
- * @num - number to be tested for prime
- * Return: 1 if not prime and prime if prime
- */
-
-int prime(int num)
-{
-	int test;
-
-	test = 2;
-
-	while (num % test != 0)
-		test++;
-
-	if (test == num)
-		return (num);
-	else
-		return (1);
-}
-
-/**
- * hcpf - highest common prime factor
- * @num - number to be factored
- * Return: hcpf
- */
-
-int hcpf(long num)
-{
-	int factor;
-
-	factor = 2;
-
-	while (factor <= num)
-	{
-		int pnum;
-
-		pnum = prime(factor);
-
-		if (pnum != 1)
-		{
-			while (num % factor == 0 && num != factor)
-				num = num / factor;
-		}
-
-		if (factor == num)
-		{
-                        printf("%ld\n", num);
-			break;
-		}
-		factor++;
-	}
-	return (0);
-
-
-
-}
-
-/**
- * main - checker
+ * main - prime numbers
+ *
  * Return: 0
  */
 
 int main(void)
 {
-	hcpf(612852475143);
+	long num = 612852475143;
+	long divisor = 2;
+	long larg_prim = 0;
+
+	while (num != 1)
+	{
+		if (num % divisor == 0)
+		{
+			num = num / divisor;
+			larg_prim = divisor;
+		}
+		divisor += 1;
+	}
+	printf("%ld\n", larg_prim);
 	return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 
-/*
- * This function uses putchar to
- * print number of long values gan
- */
-
 /**
- * print_number - main code here
- * @n: collect number for me jare
- * Return: void
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
  */
-
 
 void print_number(int n)
 {
+	unsigned int x;
+
+	x = n;
 	if (n < 0)
 	{
-		putchar('-');
-		n = -n;
+		_putchar(45);
+		x = -n;
 	}
-
-	if (n / 10)
-		print_number(n / 10);
-
-	putchar((n % 10) + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
