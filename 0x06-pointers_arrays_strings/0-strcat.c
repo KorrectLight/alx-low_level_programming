26e7720b1e1f674cb42a3eb1c9509c8d8da82d08
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-16 14:24:09 -0700

    low level programming

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - main code
+ * @dest: first input
+ * @src: second input
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0');
+		i++;
+	
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
