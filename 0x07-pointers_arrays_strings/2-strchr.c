4c06abe0680aa47dfee158a8b990c40c645c304d
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-20 08:47:16 -0700

    low level programming

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _memset - a function that locates a character
@@ -6,14 +7,17 @@
  * @c: input character to locate in string
  * Return: s when a character is found otherwise NULL
  */
+
 char *_strchr(char *s, char c)
 {
+
 	while (*s)
 	{
 		if (c == *s)
 			return (s);
 		s++;
 	}
-	else if (c != *s)
-		return (NULL);
+	if (c != *s)
+
+	return (NULL);
 }
