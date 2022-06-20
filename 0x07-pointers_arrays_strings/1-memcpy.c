6a828449a9d5a53004badfcb7a07ba53390fd0f2
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-20 15:50:20 -0700

    low level programming

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 
 /**
- * _memcpy() - a function that copies byte from one memory area to another
- * @src: address of the memory area
- * @n: numbers of byte to fill with
- * @dest: address of the memory area to copying to
- * Return: mem
+ * *_memcpy - copies memory area
+ * @dest: memory area
+ * @src: source
+ * @n: length of src
+ *
+ * Return: pointer to dest.
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	return (dest);
 }
