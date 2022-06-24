385a77a7c6d8e3d61e6d03d8e65f4b8245ec2ea9
Ezekiel Komolafe <Komolafeezekiel29@gmail.com>
2022-06-24 10:43:30 -0700

    low level programming

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Shows output if c is a lower case or otherwise
+ * @c: this is the input
+ * Return: 1 for lower case. 0 for the rest
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - main code
+ * @n: input
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}
